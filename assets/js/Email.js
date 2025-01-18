2f5999db59ac84ceafbacd5be20c5238f7380bb8
Jatin <jatin11.navani@gmail.com>
2025-01-19 02:25:01 +0530

    test

--- assets/js/Email.js
+++ assets/js/Email.js
@@ -1,31 +1,50 @@
-// Initialize EmailJS with your user ID
+// Initialize EmailJS
 emailjs.init(process.env.REACT_APP_EMAILJS_USER_ID);
 
-
-document.getElementById('contact-form').addEventListener('submit', function (event) {
+// Handle form submission
+document.getElementById('contact-form').addEventListener('submit', function(event) {
   event.preventDefault();
 
-  // Collect form data
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const message = document.getElementById('message').value;
-
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var message = document.getElementById('message').value;
 
   // Use the EmailJS send function
-  emailjs
-    .send(
-      process.env.REACT_APP_EMAILJS_SERVICE_ID,
-      process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
-      {
-        name: name,
-    email: email,
-    message: message
-  })
+  emailjs.send(
+    process.env.REACT_APP_EMAILJS_SERVICE_ID,
+    process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+    {
+      name: name,
+      email: email,
+      message: message
+    }
+  )
   .then(function(response) {
-    alert('Message sent successfully');
-    // Clear the form fields after successful submission
+    showPopup('Message sent successfully');
     document.getElementById('contact-form').reset();
   }, function(error) {
-    alert('Failed to send message: ');
+    showPopup('Failed to send message. Please try again.');
   });
 });
+
+// Function to show the popup
+function showPopup(message) {
+  var modal = document.getElementById('popup-modal');
+  var popupMessage = document.getElementById('popup-message');
+  var closeButton = document.getElementById('close-button');
+
+  popupMessage.textContent = message;
+  modal.style.display = 'block';
+
+  // Close modal on clicking the close button
+  closeButton.onclick = function() {
+    modal.style.display = 'none';
+  };
+
+  // Close modal when clicking outside the modal content
+  window.onclick = function(event) {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  };
+}
