d815cf2c377308c96596cc78ecbdf7f19496a129
Jatin <jatin11.navani@gmail.com>
2025-01-19 13:46:37 +0530

    Updated env variables

--- assets/js/Email.js
+++ assets/js/Email.js
@@ -1,23 +1,23 @@
-// Initialize EmailJS with your user ID
-emailjs.init(process.env.REACT_APP_EMAILJS_USER_ID);
+// Initialize EmailJS with your user ID from the dynamically injected config.js
+emailjs.init(window.env.REACT_APP_EMAILJS_USER_ID);
 
 // Handle form submission
 document.getElementById('contact-form').addEventListener('submit', function(event) {
   event.preventDefault();
 
   // Collect form data
-  var name = document.getElementById('name').value;
-  var email = document.getElementById('email').value;
-  var message = document.getElementById('message').value;
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
 
   // Use the EmailJS send function
   emailjs.send(
-    process.env.REACT_APP_EMAILJS_SERVICE_ID, 
-    process.env.REACT_APP_EMAILJS_TEMPLATE_ID, 
+    window.env.REACT_APP_EMAILJS_SERVICE_ID, 
+    window.env.REACT_APP_EMAILJS_TEMPLATE_ID, 
     {
       name: name,
       email: email,
-      message: message
+      message: message,
     }
   )
   .then(function(response) {
