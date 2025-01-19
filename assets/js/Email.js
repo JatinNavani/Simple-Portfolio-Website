0cfd19d783904b39bf85b88b883e10399e9e4186
Jatin <jatin11.navani@gmail.com>
2025-01-19 14:03:01 +0530

    Env variables fix

--- assets/js/Email.js
+++ assets/js/Email.js
@@ -1,29 +1,30 @@
-// Initialize EmailJS with your user ID from the dynamically injected config.js
-emailjs.init(window.env.REACT_APP_EMAILJS_USER_ID);
+const userID = window.env.REACT_APP_EMAILJS_USER_ID;
+const serviceID = window.env.REACT_APP_EMAILJS_SERVICE_ID;
+const templateID = window.env.REACT_APP_EMAILJS_TEMPLATE_ID;
 
-// Handle form submission
-document.getElementById('contact-form').addEventListener('submit', function(event) {
+// Initialize EmailJS with your user ID from the dynamically injected config.js
+emailjs.init(userID);
+
+document.getElementById('contact-form').addEventListener('submit', function (event) {
   event.preventDefault();
 
-  // Collect form data
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   const message = document.getElementById('message').value;
 
-  // Use the EmailJS send function
-  emailjs.send(
-    window.env.REACT_APP_EMAILJS_SERVICE_ID, 
-    window.env.REACT_APP_EMAILJS_TEMPLATE_ID, 
-    {
+  emailjs
+    .send(serviceID, templateID, {
       name: name,
       email: email,
       message: message,
-    }
-  )
-  .then(function(response) {
-    alert('Message sent successfully');
-    document.getElementById('contact-form').reset();
-  }, function(error) {
-    alert('Failed to send message');
-  });
+    })
+    .then(
+      function (response) {
+        alert('Message sent successfully');
+        document.getElementById('contact-form').reset();
+      },
+      function (error) {
+        alert('Failed to send message');
+      }
+    );
 });
